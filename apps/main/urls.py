2381260a62396f0087f71e0eae54cde131c7a4c0
Sultan-html <aik709470@gmail.com>
2024-10-24 22:08:04 +0600

    настроил event и event_detail а также blog и blog_detail

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from apps.main.views import main
+from apps.main.views import main,event,artist,event_detail,blog,blog_detail
 
 urlpatterns = [
-    path('', main, name='main')
+    path('', main, name='main'),
+    path('artist/',artist, name='artist'),
+    path('event/', event, name='event'),
+    path('event-detail/<int:id>/', event_detail, name='event_detail'),
+    path('blog/',blog, name='blog' ),
+    path('blog-detail/<int:id>/', blog_detail, name='blog_detail'),
 ]
